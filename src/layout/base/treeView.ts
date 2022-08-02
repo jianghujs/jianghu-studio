584b00322d15e6f398712a9e26e1d9292c266621
case <7795958+lipangpang251@user.noreply.gitee.com>
2022-08-02 09:24:01 +0800

    升级识别

--- src/layout/base/treeView.ts
+++ src/layout/base/treeView.ts
@@ -2,7 +2,7 @@
  * @Author: case 7795958+lipangpang251@user.noreply.gitee.com
  * @Date: 2022-06-11 19:30:28
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-06-25 23:00:10
+ * @LastEditTime: 2022-08-02 09:23:27
  * @FilePath: \framework\src\layout\base\treeView.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -57,7 +57,7 @@ export class BaseTreeView {
     const configPath = path.join(dir, "config/config.local.js");
     const packageJsonPath = path.join(dir, "package.json");
     if (this.pathExists(packageJsonPath) && this.pathExists(configPath)) {
-      if (!fs.readFileSync(packageJsonPath, "utf-8").includes("egg-jianghu")) {
+      if (!fs.readFileSync(packageJsonPath, "utf-8").includes("egg-jianghu") && !fs.readFileSync(packageJsonPath, "utf-8").includes("@jianghujs/jianghu")) {
         return;
       }
       this.configList.push({ ...this.getDepsInConfigJs(configPath), dir });
